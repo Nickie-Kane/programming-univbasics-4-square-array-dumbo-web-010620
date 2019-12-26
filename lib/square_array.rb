0a63c689fe91bc3078e900f2842e85a6b360b74c
nickie kane <kameo712@gmail.com>
2019-12-26 00:46:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,11 @@
 def square_array(array)
   count = 0 
   while count < array.length
-     squ = array[count]
+     squ = array[count] ** 2 
      new_array << squ
   count += 1 
   end
+  return new_array
 end
 
 numbers = [1,2,3]
